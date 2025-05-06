096ce8612f7f188661e17a91bab67890c0dc5427
Anne O'Leary <anne272@gmail.com>
2025-05-07 00:28:09 +0100

    Refactoring of async process and LLM routes - working but not fully tidy yet.

--- app/blueprints/__init__.py
+++ app/blueprints/__init__.py
@@ -2,6 +2,7 @@
 from .stories import stories_bp
 from .templates import templates_bp
 from .questions import questions_bp
+from .llm import llm_bp
 # from .story_builder import story_builder_bp
 # from .categories import categories_bp
 # from .llm_evaluation import llm_evaluation_bp
@@ -12,6 +13,7 @@ def register_blueprints(app):
     app.register_blueprint(stories_bp)
     app.register_blueprint(templates_bp)
     app.register_blueprint(questions_bp)
+    app.register_blueprint(llm_bp)
     # app.register_blueprint(story_builder_bp)
     # app.register_blueprint(llm_evaluation_bp)
     app.register_blueprint(main_bp)
