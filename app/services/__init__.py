1cf71f624d0f87d0a508099f656f6580c264e408
Anne O'Leary <anne272@gmail.com>
2025-05-07 09:56:55 +0100

    fixed Ruff linting complaint re services imports

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,12 +1,9 @@
-from .prompt_service import *
-from .story_service import *
-from .question_service import *
-from .response_service import *
-from .async_service import *
-from .llm_service import *
+from . import prompt_service
+from . import story_service
+from . import question_service
+from . import response_service
+from . import async_service
+from . import llm_service
 
-
-def get_all_services():
-    """Return a list of all service module names for documentation purposes"""
-    return ['prompt_service', 'story_service', 'question_service', 
+__all__ = ['prompt_service', 'story_service', 'question_service', 
             'response_service', 'async_service', 'llm_service']
